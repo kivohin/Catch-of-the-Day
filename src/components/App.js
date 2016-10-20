0674ecdaa2bef73653939683c0b221bcac155439
Anthony Nabuurs <kivohin@protonmail.com>
2016-10-19 20:34:40 -0400

    Finish adding all the instructions from video 13 - Understanding State.

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,38 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-    render() {
-        return(
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market" />
-                </div>
-                <Order />
-                <Inventory />
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    // Add addFish function to our state
+    this.addFish = this.addFish.bind(this);
+    // Set our empty state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // Make a copy of our state
+    const fishes = {...this.state.fishes};
+    // Add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Set state
+    this.setState({ fishes: fishes });
+  }
+
+  render() {
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    )
+  }
 }
 
 export default App;
