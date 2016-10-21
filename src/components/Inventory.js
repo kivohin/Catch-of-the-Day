1d3e76489af71aef05dcb27a15202ee9c7d9769b
Anthony Nabuurs <kivohin@protonmail.com>
2016-10-20 20:59:44 -0400

    Finish adding all the instructions from video 14 and 15.

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,7 +6,8 @@ class Inventory extends React.Component {
 		return(
 			<div>
         <h2>Inventory</h2>
-        <AddFishForm addFish={this.props.addFish} />       
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>       
       </div>
 		)
 	}
